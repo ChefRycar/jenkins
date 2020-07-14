2c445900f081e70066894990de9a90cb99c4bc64
Nick Rycar <nickrycar@Nicks-MacBook-Pro.local>
2020-07-14 11:52:49 -0700

    Updating jenkins version and jre dep

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=jenkins
 pkg_origin=workshop
-pkg_version=2.176.2
+pkg_version=2.235.1
 pkg_maintainer="Nick Rycar <nrycar@chef.io>"
 pkg_description="Extending the Jenkins package developed by Indellient & Chef Training"
 pkg_license=('mit')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="33a6c3161cf8de9c8729fd83914d781319fd1569acf487c7b1121681dba190a5"
-pkg_deps=(core/jre8 core/curl core/git)
+pkg_shasum="6375eb3a09bfde4c1fb3a7296985e225bfe2007153990a8b587e3f5f926f675d"
+pkg_deps=(core/openjdk11 core/curl core/git)
 pkg_svc_user="root"
 
 pkg_exports=(
